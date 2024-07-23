f8fc3ec21b2a381a8f6a32659dd41592fb313d70
wharris23 <147620144+wharris23@users.noreply.github.com>
2024-07-23 12:20:04 -0400

    Wyatt Harris decode file commit

--- KO_main.py
+++ KO_main.py
@@ -1,20 +1,16 @@
 def encode(password):
-    new = []
-    for num in password:
-        number = int(num)
-        new.append(number)
-
     encoded = ""
-    for val in new:
-        if val < 7:
-            val += 3
-            encoded += str(val)
-        else:
-            if val == 7:
-                val = "0"
-            elif val == 8:
-                val = "1"
-            elif val == 9:
-                val = "2"
-            encoded += str(val)
-    return "".join(encoded)
+    for char in password:
+        num = int(char)
+        new_num = (num + 3) % 10
+        encoded += str(new_num)
+    return encoded
+
+
+def decode(password):
+    decoded = ""
+    for char in password:
+        num = int(char)
+        new_num = (num - 3) % 10
+        decoded += str(new_num)
+    return decoded
